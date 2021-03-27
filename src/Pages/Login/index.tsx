e9516a544dcfa326b70ab7eeedc8ca7dafa321ba
Fernando Lopes <fernandolopes_38@hotmail.com>
2021-03-27 12:13:48 -0300

    pagina login redirecionado para dashboard

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -3,63 +3,74 @@ import { Link, useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { FaArrowRight } from 'react-icons/fa';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as yup from 'yup';
 
 import Button from '../../components/Button';
 import Header from '../../components/Header';
-
-import api from '../../services/api';
-import { UserResponse } from '../../types/user';
-import updateReduxState from '../../services/updateReduxState';
 import Loader from '../../components/Loader';
 import Input from '../../components/Input';
-import { FormHandles } from '@unform/core';
+
+import api from '../../services/api';
+import updateReduxState from '../../services/updateReduxState';
 import getValidationErrors from '../../utils/getValidationErrors';
+
+import { UserResponse } from '../../types/user';
 import { AnyObject } from '../../types/utils';
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
-  const history = useHistory();
   const formRef = useRef<FormHandles>(null);
+  const history = useHistory();
 
   const handleSubmit = useCallback(async (data: AnyObject) => {
     const filteredData: AnyObject = {}
+
+    console.log('data', data);
 
     Object.keys(data).forEach(key => {
       filteredData[key] = data[key].trim();
     });
 
+    console.log('filteredData', filteredData);
+
     setLoading(true);
+
     try {
       formRef.current?.setErrors({});
 
       const schema = yup.object().shape({
-        username: yup.string().required('Nome de usuário obrigatório '),
+        username: yup.string().required('Nome de usuário obrigatório'),
         password: yup.string().required('Senha obrigatória'),
       });
 
       await schema.validate(filteredData, {
         abortEarly: false
-      })
+      });
 
       const { data: response } = await api.post<UserResponse>('/login', {
         "usuario": username,
         "senha": password
       });
+
+      console.log('response', response);
+
       localStorage.setItem('@token_user', response.token);
       localStorage.setItem('@user_name', response.usuario.nome);
       updateReduxState();
       toast.success('Seja bem-vindo(a)');
-      //redirecionar para dashboard;
-    }
-    catch (err) {
+
+      history.push('/dashboard');
+    } catch (err) {
+      console.log('err', err);
       const errors = getValidationErrors(err);
+      console.log('errors', errors);
       formRef.current?.setErrors(errors);
       if (Object.keys(err).includes('isAxiosError')) {
         toast.error('Ocorreu algum erro!');
-        return //redirecionar para página de erro;
+        return history.push('/error');
       }
       setLoading(false);
       toast.error('Usuario ou senha incorretos!');
@@ -75,16 +86,17 @@ const Login: React.FC = () => {
       <Form ref={formRef} onSubmit={handleSubmit}>
         Faça o login
 
-          <Input name="username" value={username} onChange={e => setUsername(e.target.value)} placeholder="Digite seu usuário" autoFocus />
+        <Input name="username" value={username} onChange={e => setUsername(e.target.value)} placeholder="Digite seu usuário" autoFocus />
         <Input name="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Digite sua senha" type="password" />
 
-        {loading ? <Loader /> : <Button
-          type="submit"
-          text="Continuar"
-          Icon={FaArrowRight}
-          className="form-button"
-        />
-        }
+        {loading ? 
+          <Loader /> :
+          <Button
+            type="submit"
+            text="Continuar"
+            Icon={FaArrowRight}
+            className="form-button"
+          />}
 
         <Link to="/recover">Esqueci minha senha</Link>
         <Link to="/">Ainda não sou cliente</Link>
