43b7e139af422cc643bfd21409e62f85116b9c2c
gnatal <guilhermenatal47@gmail.com>
2021-03-28 17:47:23 -0300

    Adding card to the payment page

---
+++ src/components/FormCardBackground/index.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const FormCard = styled.div`
+  max-width: 400px;
+  width: 80vw;
+  margin: 10px auto;
+  position: relative;
+  background: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
+  padding: 20px;
+  border-radius: 20px;
+
+  color: #222;
+`
